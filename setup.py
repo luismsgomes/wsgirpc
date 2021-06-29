74229bb41b4caeea14aa5a9558baa4169639fe9d
Luís Gomes <luismsgomes@gmail.com>
2021-06-29 18:07:20 +0100

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url="https://github.com/luismsgomes/wsgirpc",
     author="Luís Gomes",
     author_email="luismsgomes@gmail.com",
-    version="0.1.0",
+    version="1.0.0",
     package_dir={"": "src"},
     py_modules=["wsgirpc"],
     install_requires=["requests"],
--- src/wsgirpc.py
+++ src/wsgirpc.py
@@ -3,7 +3,7 @@ import queue
 import requests
 
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 
 class InvalidRequest(Exception):
